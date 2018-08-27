4970103fdb1841cc799fa2041f391eef509adbf5
NauAlex93 <naualex93@mail.ru>
2018-08-27 23:43:26 +0300

    Added equals & hashCode in Resume class, static class members, introduce interface Storage.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,8 +3,10 @@ import java.util.Arrays;
 /**
  * Array based storage for Resumes
  */
-public class ArrayStorage {
-    private Resume[] storage = new Resume[10000];
+public class ArrayStorage implements Storage {
+    private static final int STORAGE_LIMIT = 10000;
+
+    private Resume[] storage = new Resume[STORAGE_LIMIT];
     private int size = 0;
 
     public void clear() {
@@ -13,7 +15,7 @@ public class ArrayStorage {
     }
 
     public void save(Resume resume) {
-        if (storage.length == size) {
+        if (size == STORAGE_LIMIT) {
             System.out.println("Storage is full.");
             return;
         }
@@ -56,9 +58,6 @@ public class ArrayStorage {
         }
     }
 
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
     public Resume[] getAll() {
         return Arrays.copyOf(storage, size);
     }
---
+++ src/Storage.java
@@ -0,0 +1,19 @@
+public interface Storage {
+
+    void clear();
+
+    void save(Resume resume);
+
+    Resume get(String uuid);
+
+    void update(Resume resume);
+
+    void delete(String uuid);
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    Resume[] getAll();
+
+    int size();
+}
