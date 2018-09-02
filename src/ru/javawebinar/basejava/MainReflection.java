ce0678993f29e970fcc3a41bec574da6f94bcad9
NauAlex93 <naualex93@mail.ru>
2018-09-02 22:33:43 +0300

    realized toString via ReflectionToStringBuilder

--- src/ru/javawebinar/basejava/MainReflection.java
+++ src/ru/javawebinar/basejava/MainReflection.java
@@ -3,6 +3,7 @@ package ru.javawebinar.basejava;
 import ru.javawebinar.basejava.model.Resume;
 
 import java.lang.reflect.Field;
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 
 public class MainReflection {
     public static void main(String[] args) throws IllegalAccessException {
@@ -13,5 +14,6 @@ public class MainReflection {
         System.out.println(field.get(resume));
         field.set(resume, "new_uuid");
         System.out.println(resume);
+        System.out.println(ReflectionToStringBuilder.toString(resume));
     }
 }
